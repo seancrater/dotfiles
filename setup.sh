ae3d60a0b5edb1751d6ed1b0b01e18532339f4d8
seancrater <sean@seancrater.com>
2019-03-01 15:31:23 -0500

    Adding emojis and echos

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! $(which brew)
+then
+  echo "Installing Homebrew ⚙️"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+fi
 
 # Install Command Line Tools
+echo "Installing Xcode developer utils 📱"
 xcode-select --install
 
 # Install VSCode
+echo "Installing Visual Studio Code ⌨️"
 brew cask install visual-studio-code
 
 # Copy dotfiles
+echo "Copying Git & Zsh configs 📀"
 cp .gitconfig ~/
 cp .zshrc ~/
